6d01a354196eaae6c62bd8c17fdb05305e320ba9
Thomas Makin <halorocker89@gmail.com>
2024-04-24 12:07:32 -0400

    rollup: fix output settings

--- rollup.config.js
+++ rollup.config.js
@@ -13,13 +13,11 @@ export default {
             file: 'dist/index.js',
             format: 'cjs',
             sourcemap: true,
-            exports: "named",
         },
         {
             file: 'dist/index.es.js',
             format: 'es',
             sourcemap: true,
-            exports: "named",
         },
     ],
     plugins: [
