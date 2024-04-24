8cd548a72b624813c847b9264adf023b9382b608
Thomas Makin <halorocker89@gmail.com>
2024-04-24 11:51:51 -0400

    update for react 18

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,44 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
+import terser from '@rollup/plugin-terser'
 import autoprefixer from 'autoprefixer'
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.es.js',
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true,
-      minimize: true,
-      plugins: [autoprefixer()],
-    }),
-    url(),
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true,
-    }),
-    resolve(),
-    commonjs(),
-    process.env.NODE_ENV === 'production' && terser(),
-  ],
+    input: 'src/index.js',
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'cjs',
+            sourcemap: true,
+            exports: "named",
+        },
+        {
+            file: 'dist/index.es.js',
+            format: 'es',
+            sourcemap: true,
+            exports: "named",
+        },
+    ],
+    plugins: [
+        postcss({
+            modules: true,
+            minimize: true,
+            plugins: [autoprefixer()],
+        }),
+        url(),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'runtime',
+        }),
+        resolve(),
+        commonjs(),
+        process.env.NODE_ENV === 'production' && terser(),
+    ],
+    external: [
+        "react",
+        "react-dom",
+    ]
 }
